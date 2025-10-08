775b65bed936ff8ef656de1d333b0fa52919e162
Jessewhalen1992 <jesse_whalen@hotmail.com>
2025-10-08 11:10:36 -0300

    Add ResidenceSync AutoCAD plugin solution

---
+++ ResidenceSync/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ResidenceSync")]
+[assembly: AssemblyDescription("Residence synchronization tools for AutoCAD.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ResidenceSync")]
+[assembly: AssemblyProduct("ResidenceSync")]
+[assembly: AssemblyCopyright("Copyright © ResidenceSync 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d6e1d6c-7235-4fbb-8bb5-5a1f9deecb3a")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
